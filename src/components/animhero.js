f4f8a3eb86cef86c5d8a2794d448e3097baf4126
Tech_1 <tech_1@IMac-de-Nico.local>
2019-08-29 09:34:06 +0200

    hero OK

---
+++ src/components/animhero.js
@@ -0,0 +1,139 @@
+import React from "react";
+import posed from "react-pose";
+
+const Orbs = posed.div({
+    open: { 
+        staggerChildren: 150, 
+    },
+    closed: {}
+});
+const Blue = posed.span({
+    open: { 
+        y: 0, 
+        opacity: 1,
+        transition: {
+            duration: 500,
+            ease: 'linear'
+        }
+    },
+    closed: { 
+        y: 600, 
+        opacity: 1,
+        transition: {
+            duration: 500,
+            ease: 'linear'
+        }
+    },
+    initialPose: 'closed'
+});
+
+const Top = posed.span({
+    open: { 
+        y: 0, 
+        opacity: 1, 
+        transition: {
+            duration: 400,
+            ease: 'linear'
+          }
+        
+    },
+    closed: { 
+        y: -600, 
+        opacity: 1,
+        transition: {
+            duration: 400,
+            ease: 'linear'
+        }
+    },
+    initialPose: 'closed'
+});
+
+const BigTitle = posed.h1({
+    open: { 
+        y: 0, 
+        delay: 1300,
+        opacity: 1, 
+        transition: {
+            duration: 400,
+            ease: 'linear'
+          }
+        
+    },
+    closed: { 
+        y: -600, 
+        opacity: 1,
+        transition: {
+            duration: 400,
+            ease: 'linear'
+        }
+    },
+    initialPose: 'closed'
+});
+const SmallTitle = posed.h2({
+    open: { 
+        x: 0, 
+        opacity: 1, 
+        delay: 1500,
+        transition: {
+            duration: 800,
+            ease: 'linear'
+          }
+        
+    },
+    closed: { 
+        x: 800, 
+        opacity: 1,
+        transition: {
+            duration: 800,
+            ease: 'linear'
+        }
+    },
+    initialPose: 'closed'
+});
+
+
+
+
+class Hero extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({isOpen: true});
+        }, 1500);
+    }
+    componentWillUpdate() {
+        setTimeout(() => {
+            this.setState({isOpen: true});
+        }, 1500);
+    }
+    
+
+    render() {
+        return (
+        <section className="hero flex items-center justify-end ">
+            <div className="max-w-6xl w-full mx-auto flex-col items-end">
+                <Orbs className="orbs flex flex-1 justify-end" pose={this.state.isOpen ? 'open' : 'closed'}> 
+                    <Top className="top text-white century" >l</Top> 
+                    <Top className="top text-white century" >i</Top> 
+                    <Top className="top text-white century" >n</Top> 
+                    <Top className="top text-white century" >k</Top> 
+                    <Blue className="blue text-bleu" >/</Blue> 
+                    <Top className="top text-white century" >w</Top> 
+                    <Top className="top text-white century" >e</Top> 
+                    <Top className="top text-white century" >b</Top> 
+                </Orbs>
+                <BigTitle className="flex flex-1 bigtitle text-white justify-end century" pose={this.state.isOpen ? 'open' : 'closed'}>Agence web à Toulouse et Agen</BigTitle>
+                <SmallTitle className="flex flex-1 smalltitle text-white justify-end century" pose={this.state.isOpen ? 'open' : 'closed'}>Connectez-vous avec le reste du monde</SmallTitle>
+            </div>
+        </section>
+        )
+    }
+}
+
+  export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,20 @@
 import React from "react";
-
+import posed from 'react-pose';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+import Hero from "../components/animhero";
 import "../scss/hero.scss";
 
 
 function IndexPage() {
+
   return (
     <Layout>
       <SEO
         title="Home"
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
       /> 
-      <section className="hero flex items-center justify-end">
-        <span className="orbs flex flex-1 justify-end max-w-6xl mx-auto"> 
-          <span className="top text-white century" >l</span> 
-          <span className="top text-white century" >i</span> 
-          <span className="top text-white century" >n</span> 
-          <span className="top text-white century" >k</span> 
-          <span className="blue text-bleu" >/</span> 
-          <span className="top text-white century" >w</span> 
-          <span className="top text-white century" >e</span> 
-          <span className="top text-white century" >b</span> 
-        </span>
-        
-      </section>
+      <Hero></Hero>
       <section className="max-w-4xl mx-auto px-4">
 
       </section>
