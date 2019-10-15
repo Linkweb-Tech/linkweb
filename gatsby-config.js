c956bfdc6746f1247ca2cc28e6d364a33e0b74d1
Linkweb Référencement <linkweb@iMac-de-Julien-Ref.local>
2019-10-15 09:30:33 +0200

    test analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -157,9 +157,9 @@ module.exports = {
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
+        // anonymize: true,
+        // // Setting this parameter is also optional
+        // respectDNT: true,
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         sampleRate: 5,
