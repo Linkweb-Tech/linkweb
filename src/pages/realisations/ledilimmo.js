293378a62dbe4eb1eee0e4f1413ad4cf1d8c144c
Linkweb Référencement <linkweb@iMac-de-Julien-Ref.local>
2019-11-21 11:14:23 +0100

    correction lien ledilimmo

--- src/pages/realisations/ledilimmo.js
+++ src/pages/realisations/ledilimmo.js
@@ -62,7 +62,7 @@ class ledilimmo extends React.Component {
                             </h1>
                             <h3 data-aos="fade-up" className="italic text-center text-lg sm:text-2xl text-white my-12">Mettre en place un outil permettant d’apporter de la visibilité à l’offre immobilière de Ledil.immo, tout en proposant une solution web pour la gestion du réseau de mandataires.</h3>
                             <br/>
-                            <a data-aos="fade-down" href="/realisations/ledillimo/#start"><svg width="80px" viewBox="0 0 100 125"><g><g><path fill="#FFFFFF" d="M59.3,46.5l-8.7,8.7l-8.7-8.7c-0.8-0.8-2-0.8-2.8,0c-0.8,0.8-0.8,2,0,2.8l10.1,10.1c0.4,0.4,0.9,0.6,1.4,0.6    s1-0.2,1.4-0.6l10.1-10.1c0.8-0.8,0.8-2,0-2.8C61.4,45.7,60.1,45.7,59.3,46.5z M50.8,5c-24.9,0-45,20.1-45,45    c0,24.9,20.1,45,45,45c24.9,0,45-20.1,45-45C95.8,25.1,75.6,5,50.8,5z M50.8,91c-22.6,0-41-18.4-41-41s18.4-41,41-41    s41,18.4,41,41S73.4,91,50.8,91z"/></g></g></svg></a>
+                            <a data-aos="fade-down" href="/realisations/ledilimmo/#start"><svg width="80px" viewBox="0 0 100 125"><g><g><path fill="#FFFFFF" d="M59.3,46.5l-8.7,8.7l-8.7-8.7c-0.8-0.8-2-0.8-2.8,0c-0.8,0.8-0.8,2,0,2.8l10.1,10.1c0.4,0.4,0.9,0.6,1.4,0.6    s1-0.2,1.4-0.6l10.1-10.1c0.8-0.8,0.8-2,0-2.8C61.4,45.7,60.1,45.7,59.3,46.5z M50.8,5c-24.9,0-45,20.1-45,45    c0,24.9,20.1,45,45,45c24.9,0,45-20.1,45-45C95.8,25.1,75.6,5,50.8,5z M50.8,91c-22.6,0-41-18.4-41-41s18.4-41,41-41    s41,18.4,41,41S73.4,91,50.8,91z"/></g></g></svg></a>
                     </section>
                     <section className="w-full flex flex-col md:flex-row mt-12">
                         <div className="w-full md:w-11/12 px-4 py-2 md:py-6 my-2 ml-0 md:ml-24 lg:ml-32 md:my-4 flex flex-col order-last md:order-first">
