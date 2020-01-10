d95e26e4cfcbe5996cea6454c2af07b21bc1ae59
Linkweb Référencement <linkweb@iMac-de-Julien-Ref.local>
2020-01-10 11:09:56 +0100

    correction onload fonction 4

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,7 @@ import CookieConsent from 'react-cookie-consent';
 import Header from "./header";
 import Footer from "./footer";
 import "../scss/global.scss";
-import tarteaucitron from "../../static/data/tarteaucitron/tarteaucitron.js";
-import "../../static/data/tarteaucitron/css/tarteaucitron.css";
+
 
 
 
@@ -22,29 +21,29 @@ class Layout extends React.Component {
 
   componentWillMount() {
     
-    tarteaucitron.init({
-      "privacyUrl": "", /* Privacy policy url */
+    // tarteaucitron.init({
+    //   "privacyUrl": "", /* Privacy policy url */
     
-      "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
-      "cookieName": "tarteaucitron", /* Cookie name */
+    //   "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
+    //   "cookieName": "tarteaucitron", /* Cookie name */
       
-      "orientation": "bottom", /* Banner position (top - bottom) */
-      "showAlertSmall": true, /* Show the small banner on bottom right */
-      "cookieslist": true, /* Show the cookie list */
+    //   "orientation": "bottom", /* Banner position (top - bottom) */
+    //   "showAlertSmall": true, /* Show the small banner on bottom right */
+    //   "cookieslist": true, /* Show the cookie list */
     
-      "adblocker": false, /* Show a Warning if an adblocker is detected */
-      "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
-      "highPrivacy": true, /* Disable auto consent */
-      "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+    //   "adblocker": false, /* Show a Warning if an adblocker is detected */
+    //   "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
+    //   "highPrivacy": true, /* Disable auto consent */
+    //   "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
     
-      "removeCredit": false, /* Remove credit link */
-      "moreInfoLink": true, /* Show more info link */
-      "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
+    //   "removeCredit": false, /* Remove credit link */
+    //   "moreInfoLink": true, /* Show more info link */
+    //   "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
     
-      //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for multisite */
+    //   //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for multisite */
                             
-      "readmoreLink": "/cookiespolicy" /* Change the default readmore link */
-      });
+    //   "readmoreLink": "/cookiespolicy" /* Change the default readmore link */
+    //   });
 
 
  
