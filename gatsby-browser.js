20d4555bad7588ac6255bac4b5894e33d9a633df
Linkweb-Tech <technique@linkweb.fr>
2020-03-03 21:55:39 +0100

    RGPD style3

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,25 +13,5 @@ export const onClientEntry = () => {
       AOS.init(
          { duration: 2000 }
       );
-
-      // let scriptTarte = document.createElement('script');
-      // scriptTarte.type = "text/javascript";
-      // let tartContent = document.createTextNode(
-
-      //    tarteaucitron.user.gajsUa = 'UA-43540005-1',
-      //    tarteaucitron.user.gajsMore = function () { /* add here your optionnal _ga.push() */ },
-      //    (tarteaucitron.job = tarteaucitron.job || []).push('gajs'),
-      //    tarteaucitron.user.googletagmanagerId = 'GTM-WJMB7VQ',
-      //    (tarteaucitron.job = tarteaucitron.job || []).push('googletagmanager')
-      // );
-      // console.log(scriptTarte);
-      // scriptTarte.appendChild(tartContent);
-
-      // document.body.appendChild(scriptTarte);
-
-
-      //ici
-
-
    }
 }
