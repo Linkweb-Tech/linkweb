27ae4a427c02f0ecf26b1982818c6bdc14108aea
Linkweb-Tech <technique@linkweb.fr>
2020-03-12 17:28:48 +0100

    modification nombre avis accueil

--- src/pages/index.js
+++ src/pages/index.js
@@ -228,7 +228,7 @@ function IndexPage(props) {
             </h3>
             <h4 className="text-4xl text-white mt-0 lg:-mt-5">sur Google</h4>
             <p className="text-white font-normal">
-              Nos clients sont satisfaits du travail engagé sur leur site web.<br />Note basée sur 65 avis de clients.
+              Nos clients sont satisfaits du travail engagé sur leur site web.<br />Note basée sur 67 avis de clients.
             </p>
           </div>
         </div>
