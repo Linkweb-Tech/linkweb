bf9131138f08f19db5995c692f12442f46c7f9c8
Linkweb-Tech <technique@linkweb.fr>
2020-12-01 14:25:41 +0100

    4e tentative

--- src/components/buttonpage.js
+++ src/components/buttonpage.js
@@ -17,7 +17,7 @@ class buttonPage extends React.Component {
             <a 
             href={ this.props.url }     
             /*to={ this.props.url } */
-                className={`font-light pageButton bg-${this.props.color || 'bleu'} text-${this.props.colortext || 'black'} hover:text-bleu flex justify-center mt-3 items-center hover:bg-white text-center text-sm md:text-md lg:text-md rounded-huge px-4 py-4 mt-0`}
+                className={`font-light pageButton bg-${this.props.color || 'bleu'} text-${this.props.colortext || 'black'} hover:text-bleu flex justify-center my-3 items-center hover:bg-white text-center text-sm md:text-md lg:text-md rounded-huge px-4 py-4 mt-0`}
                 style={{minWidth:'150px'}}
             >
               {this.props.text}
