38952439035ba9ab104f3c360a30e878524bb8ba
Linkweb-Tech <61689526+Linkweb-Tech@users.noreply.github.com>
2021-04-23 14:25:53 +0200

    ajout script Tawk to

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,5 +13,15 @@ export const onClientEntry = () => {
       AOS.init(
          { duration: 2000 }
       );
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/6082b1a662662a09efc16b50/1f3v8bsdq';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+      })();
+
    }
 }
