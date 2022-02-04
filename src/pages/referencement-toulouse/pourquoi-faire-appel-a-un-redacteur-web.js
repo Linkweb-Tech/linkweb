64dc3d72cf91ba5324cb4aadcbc0bd5662b33b8e
Linkweb-Tech <technique@linkweb.fr>
2022-02-04 16:34:41 +0100

    création page pourquoi faire appel a un redacteur web

---
+++ src/pages/referencement-toulouse/pourquoi-faire-appel-a-un-redacteur-web.js
@@ -0,0 +1,175 @@
+import React, { Component } from "react";
+import { Link, graphql } from "gatsby";
+import Layout from "../../components/layout";
+// import PageTransition from "gatsby-plugin-page-transitions";
+import SEO from "../../components/seo";
+import ButtonBlog from "../../components/buttonblog";
+import "../../scss/faqpages.scss";
+import BoutonBlog from '../../components/boutonBlog.js';
+
+
+import background from "../../images/foire-aux-questions.jpg";
+import usage from "../../images/usages-site-internet.jpg";
+import redacteur from "../../images/engager-un-redacteur-web.jpg";
+import referencement from "../../images/ameliorer-son-referencement-naturel.jpg";
+
+import fbIcon from "../../images/facebook-logo.svg";
+import twitterIcon from "../../images/twitter-logo.svg";
+import instaIcon from "../../images/instagram-logo.svg";
+import bgbloc from "../../images/creation-sites-web-toulouse.jpg";
+
+
+
+
+class PourquoiFaireAppelAUnRedacteurWeb extends Component {
+
+    constructor(props) {
+        super(props);
+    
+    }
+
+render() {
+    return (
+        <Layout location={this.props.location}>
+
+            <SEO
+                title="Pourquoi faire appel à un rédacteur web ? | Agence SEO à Toulouse (31)"
+                description="La création de contenus de qualité est centrale dans le référencement. Faire appel à un rédacteur web peut s'avérer nécessaire."
+                keywords={[`Agence SEO Toulouse`, `Référencement Toulouse`]}
+                url="https://linkweb.fr/referencement-toulouse/pourquoi-faire-appel-a-un-redacteur-web/"
+                nom="Pourquoi faire appel à un rédacteur web ?"
+                slug="referencement-toulouse/pourquoi-faire-appel-a-un-redacteur-web/"
+            />
+        
+            {/* <PageTransition
+                defaultStyle={{
+                    transition: 'left 200ms cubic-bezier(0.47, 0, 0.75, 0.72)',
+                    transitionDuration: '0.5s',
+                    transitionDelay: '0.1s',
+                    //left: '100%',
+                    position: 'relative',
+                    width: '100%',
+                    zIndex: 20,
+                    minHeight: '100vh',
+                }}
+                transitionStyles={{
+                    entering: { left: '-100%' },
+                    entered: { left: '0' },
+                    exiting: { left: '-100%' },
+
+                }}
+                transitionTime={200}
+            > */}
+                <section className="w-full bloctitrespages mx-auto pb-0 px-4 pt-6 bg-black flex flex-col justify-center items-center" style={{background:'#00000094 url(' + background +')', backgroundBlendMode:'multiply', backgroundSize:'cover', backgroundPosition:'center', height:'450px'}}>
+                    <h2 data-aos="fade-down" data-aos-delay="500" className="century text-5xl sm:text-6xl text-center text-white mt-40">
+                    Pourquoi faire appel à un rédacteur web ?
+                    </h2>
+                    <h1 className="century text-xl sm:text-2xl text-center text-white" data-aos="fade-up" data-aos-delay="500">Agence SEO à Toulouse</h1>
+                    <br/>
+                    {/* Réseaux Sociaux */}
+                    <section className="w-full flex flex-row justify-center text-white ">
+                            <div className="w-1/3 md:w-1/6 lg:w-1/12 flex flex-row">
+                                <div className="w-1/3 block mx-auto" data-aos="fade-left">
+                                    <a href="https://www.facebook.com/AgenceLw" target="blank" rel="noreferrer noopener" className="flex justify-center no-underline text-white">
+                                    <img 
+                                    src={ fbIcon }
+                                    alt="Réseaux sociaux"
+                                    className="w-6 fill-current"
+                                    />
+                                    </a>
+                                </div>
+                                <div className="w-1/3 block, mx-auto" data-aos="fade-up">
+                                    <a href="https://twitter.com/AgenceLinkweb" target="blank" rel="noreferrer noopener" className="flex justify-center no-underline text-white">
+                                    <img 
+                                    src={ twitterIcon }
+                                    alt="Réseaux sociaux"
+                                    className="w-6 fill-current"
+                                    />
+                                    </a>
+                                </div>
+                                <div className="w-1/3 block, mx-auto" data-aos="fade-right">
+                                    <a href="https://instagram.com/AgenceLinkweb" target="blank" rel="noreferrer noopener" className="flex justify-center no-underline text-white">
+                                    <img 
+                                    src={ instaIcon }
+                                    alt="Réseaux sociaux"
+                                    className="w-6 fill-current"
+                                    style={{maxHeight:'50px', minWidth:'15px'}}
+                                    />
+                                    </a>
+                                </div>
+                            </div>
+                    </section>
+                    {/* FIN Réseaux Sociaux */}
+                </section>
+                
+                <section className="w-full  flex flex-col xl:flex-row flex justify-center">
+                    
+                    
+                    <div className="bloctextpages w-full lg:w-1/2 flex flex-col justify-center century text-justify">
+                        
+                        <h3>Si vous êtes comme la plupart des propriétaires d'entreprise, vous n'avez pas le temps de rédiger vous-même tout le contenu de votre site Web. C'est là qu'intervient un rédacteur web ! </h3>
+                        <br/>
+                        <p>Un <strong>rédacteur web</strong> peut vous aider à <strong className="font-bold">créer un contenu de haute qualité qui suscitera l'intérêt de vos lecteurs</strong> et contribuera à améliorer le référencement naturel de votre site web. Nous allons examiner certaines des <strong className="font-bold">raisons pour lesquelles vous devriez faire appel à un rédacteur web pour votre entreprise</strong> en vue de développer son activité.</p>
+                        <br/>
+                        <img src={redacteur} className="w-full" alt="Faire appel à un rédacteur web"/>
+                        <br/>
+                        <h2>Qu'est-ce qu'un rédacteur web SEO ?</h2>
+                        <br/>
+                        <p>L'objectif de la <strong>rédaction web</strong> est de <strong className="font-bold">susciter l'intérêt du lecteur et de le persuader d'agir</strong>. Que vous souhaitiez qu'il achète un produit, qu'il s'inscrive sur votre liste d'adresses électroniques ou qu'il visite votre site internet, une <strong>bonne rédaction Web</strong> peut vous aider à atteindre les objectifs de votre stratégie.</p>
+                        <p>Un rédacteur web professionnel peut vous aider à <strong className="font-bold">produire un contenu de haute qualité qui sera non seulement adapté à la stratégie</strong> en place sur votre site web, mais aussi bien classé dans les <strong>moteurs de recherche</strong>.</p>
+                        <p>En effet, un <strong>bon rédacteur web SEO</strong> est un professionnel qui sait écrire pour le Web et utiliser les mots-clés et expressions susceptibles d'améliorer le classement de votre site. En ce sens, <strong className="font-bold">la rédaction de contenu pertinent est essentielle dans le cadre d'une stratégie de SEO</strong>. Toutefois, il est important de comprendre que la rédaction web et la rédaction SEO ne sont pas exactement deux disciplines similaires. Néanmoins, une formation peut être effectuée pour une mise à niveau spécifique pour le <strong>SEO</strong>.</p>
+                        <br/>
+                        <h2>Quels sont les avantages de faire appel à un rédacteur web ?</h2>
+                        <br/>
+                        <p>Il existe de nombreuses raisons pour lesquelles vous devriez <strong>engager un rédacteur web</strong> pour votre entreprise et en particulier pour <strong className="font-bold">faire rédiger les textes et réaliser les contenus des articles de votre blog ou site internet</strong>. Voici quelques-uns des avantages les plus importants.</p>
+                        <br/>
+                        <h3 className="text-darkgrey" style={{fontSize:'23px'}}>Augmentation du trafic</h3>
+                        <br/>
+                        <p>En créant un <strong>contenu de haute qualité</strong>, un rédacteur web peut contribuer à augmenter le trafic sur <strong>votre site web</strong>. Ce trafic accru signifie non seulement <strong className="font-bold">plus de clients potentiels, mais aussi un meilleur classement dans les moteurs de recherche</strong>. Le métier de rédacteur web trouve particulièrement sa place dans le SEO et le travail de la <strong>visibilité sur Internet et sur le Web</strong>.</p>
+                        <br/>
+                        <h3 className="text-darkgrey" style={{fontSize:'23px'}}>Amélioration de l'engagement des clients</h3>
+                        <br/>
+                        <p>Un <strong>bon contenu rédactionnel</strong> permet de garder les gens sur votre site Web plus longtemps, ce qui signifie qu'ils sont plus susceptibles de s'intéresser à vos produits ou services. C'est tout l'enjeu du <strong>content marketing</strong>, ou marketing de contenus et des techniques qu'il implique.</p>
+                        <br/>
+                        <h3 className="text-darkgrey" style={{fontSize:'23px'}}>Une plus grande visibilité en ligne</h3>
+                        <br/>
+                        <p>Un contenu de qualité vous aidera à <strong className="font-bold">vous démarquer de la concurrence et à attirer de nouveaux clients</strong>. Rédiger du contenu éditorial et des fiches-produits optimisées est un avantage certain pour votre <strong>site e-commerce</strong>. Des contenus optimisés et pertinents sont également un plus en termes de <strong>référencement naturel</strong>. Cela fait appel à des compétences d'écriture, de création ainsi que de communication pour mener à bien une <strong>stratégie de contenu et de marketing web</strong> ou webmarketing.</p>
+                        <br/>
+                        <h3 className="text-darkgrey" style={{fontSize:'23px'}}>Une confiance et une fidélité accrues des clients</h3>
+                        <br/>
+                        <p>Lorsque les internautes trouvent des informations de qualité sur un site Web, ils sont plus susceptibles de faire confiance à cette entreprise et d'acheter chez elle. Ce type de <strong className="font-bold">contenu web contribue également à fidéliser la clientèle</strong>.</p>
+                        <br/>
+                        <h2>Faut-il externaliser la rédaction web ?</h2>
+                        <br/>
+                        <p>La réponse à cette question dépend <strong className="font-bold">de votre entreprise et de votre budget</strong>. L'externalisation de la rédaction web peut être un moyen rentable d'obtenir un <strong>contenu de qualité pour votre site web</strong> dans le cadre d'une stratégie de web marketing. Cependant, il est important de <strong className="font-bold">trouver un rédacteur qualifié en freelance</strong> qui sait comment écrire pour le web et utiliser correctement les <strong>mots-clés et les expressions de référencement</strong>.</p>
+                        <p>Également, cela dépend de votre organisation. Engager un <strong>rédacteur web salarié ou un freelance</strong> n'implique pas les mêmes méthodes de gestion. En effet, il existe des services de rédaction, comme des rédacteurs en totale autonomie. Néanmoins, il est essentiel de trouver la formule qui vous correspond car, au fur et à mesure que votre entreprise se développe, vous pourriez <strong className="font-bold">trouver nécessaire de faire appel à un rédacteur</strong>.</p>
+                        <p>Par ailleurs, faire confiance à un rédacteur ou à un rédactrice externe web freelance pour rédiger les contenus de votre blog et de votre site pour promouvoir votre activité <strong className="font-bold">n'est pas une décision à prendre à la légère</strong>. C'est pourquoi, il est important de définir une ligne éditoriale axée sur les internautes, potentiels prospects, que vous ciblez et souhaitez fidéliser.</p>
+                        <p>En définitive, il y a plusieurs raisons pour lesquelles il est important d'<strong>engager un rédacteur SEO</strong> - de l'augmentation du trafic à un meilleur engagement des clients. Non seulement, ils aideront à <strong className="font-bold">améliorer la visibilité de votre site en ligne, mais aussi sa crédibilité auprès des lecteurs</strong>.</p>
+                        <br/>
+                    </div>
+                    <div className="w-full mx-auto lg:w-1/2 xl:w-1/4 px-0 md:px-24 lg:px-10 mb-32 text-justify font-normal flex flex-col justify-start items-center lg:items-start" >
+                        <div className="bloccontactsticky w-full lg:w-full flex flex-col justify-center items-center century text-justify flex" style={{background:'#3c3c3cfa url(' + bgbloc +')', backgroundBlendMode:'overlay', backgroundSize:'cover', backgroundPosition:'center'}}>
+                                    <h2>Vous souhaitez mettre en place un projet web ?</h2>
+                                    <br/>
+                                    <h3>Linkweb met en place votre projet de création de site web.</h3>
+                                    <br/>
+                                    <div className="w-1/2 flex flex-row justify-center items-center">
+                                    <br/>
+                                        <BoutonBlog  url="/contact-agence-web-toulouse/" text="Contactez-nous" position="center" />
+                                    </div>
+                        </div>
+                    </div>
+                    
+                </section>
+
+                
+                
+            {/* </PageTransition> */}
+        </Layout>
+    );
+}
+};
+
+
+
+export default PourquoiFaireAppelAUnRedacteurWeb;
