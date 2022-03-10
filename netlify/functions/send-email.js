300015e0c572994edc3f4b2168788a3d759cffa5
Linkweb-Tech <61689526+Linkweb-Tech@users.noreply.github.com>
2022-03-10 15:46:33 +0100

    Ajout notif email

---
+++ netlify/functions/send-email.js
@@ -0,0 +1,41 @@
+const nodemailer = require("nodemailer");
+
+exports.handler = async function (event, context, callback) {
+  // Parse the JSON text received.
+  const body = JSON.parse(event.body);
+
+  // Build an HTML string to represent the body of the email to be sent.
+  const html = `<div style="margin: 20px auto;">${body.body}</div>`;
+
+  // Generate test SMTP service account from ethereal.email. Only needed if you
+  // don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "mail.linkweb.fr",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'technique@linkweb.fr', // generated ethereal user
+      pass: 'kJ26!0s0aX', // generated ethereal password
+    },
+  });
+
+  try {
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"☁️ The Cloud ☁️" <technique@linkweb.fr>',
+      to: body.email,
+      subject: "Nouveau mail",
+      text: body.body,
+      html: html,
+    });
+    // Log the result
+    console.log(info);
+    callback(null, { statusCode: 200, body: JSON.stringify(info) });
+  } catch (error) {
+    // Catch and log error.
+    callback(error);
+  }
+};
