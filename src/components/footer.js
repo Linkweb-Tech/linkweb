46dc37c96fc5cac3119372db94ef6fd4aa403e12
Linkweb-Tech <technique@linkweb.fr>
2022-03-25 09:18:19 +0100

    ajout community management dans le footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -148,6 +148,12 @@ class Footer extends Component {
                         >
                             <span className="chevronFooter">></span> Référencement Agen
                         </Link>
+                        <Link
+                            to="/agence-community-management/"
+                            className="block md:inline-block mt-4 md:mt-0 no-underline text-white century py-2 px-4"
+                        >
+                            <span className="chevronFooter">></span> Community Management
+                        </Link>
                         {/* <Link
                             to="/agence-digitale-toulouse/"
                             className="block md:inline-block mt-4 md:mt-0  no-underline text-white century py-2 px-4"
